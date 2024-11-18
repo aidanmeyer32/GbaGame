db715d974ce9996c8e5c4c8b52055fc5196ab43b
Aidan Meyer <ameyer3@mail.umw.edu>
2024-11-18 13:26:40 -0500

    removed scrolling, added sprite .c and .h

--- README.txt
+++ README.txt
@@ -1,1 +1,1 @@
-hello
+This is a simple GBA mario-like game where the player must hit as many mystery blocks possible before the time runs out.
--- parrallax.c
+++ parrallax.c
@@ -40,60 +40,16 @@ volatile unsigned long* display_control = (volatile unsigned long*) 0x4000000;
 /* the address of the color palette */
 volatile unsigned short* bg_palette = (volatile unsigned short*) 0x5000000;
 
-/* the button register holds the bits which indicate whether each button has
- * been pressed - this has got to be volatile as well
- */
-volatile unsigned short* buttons = (volatile unsigned short*) 0x04000130;
-
-/* scrolling registers for backgrounds */
-volatile short* bg0_x_scroll = (unsigned short*) 0x4000010;
-volatile short* bg0_y_scroll = (unsigned short*) 0x4000012;
-volatile short* bg1_x_scroll = (unsigned short*) 0x4000014;
-volatile short* bg1_y_scroll = (unsigned short*) 0x4000016;
-volatile short* bg2_x_scroll = (unsigned short*) 0x4000018;
-volatile short* bg2_y_scroll = (unsigned short*) 0x400001a;
-volatile short* bg3_x_scroll = (unsigned short*) 0x400001c;
-volatile short* bg3_y_scroll = (unsigned short*) 0x400001e;
-
-
-/* the bit positions indicate each button - the first bit is for A, second for
- * B, and so on, each constant below can be ANDED into the register to get the
- * status of any one button */
-#define BUTTON_A (1 << 0)
-#define BUTTON_B (1 << 1)
-#define BUTTON_SELECT (1 << 2)
-#define BUTTON_START (1 << 3)
-#define BUTTON_RIGHT (1 << 4)
-#define BUTTON_LEFT (1 << 5)
-#define BUTTON_UP (1 << 6)
-#define BUTTON_DOWN (1 << 7)
-#define BUTTON_R (1 << 8)
-#define BUTTON_L (1 << 9)
-
 /* the scanline counter is a memory cell which is updated to indicate how
  * much of the screen has been drawn */
 volatile unsigned short* scanline_counter = (volatile unsigned short*) 0x4000006;
+
 
 /* wait for the screen to be fully drawn so we can do something during vblank */
 void wait_vblank() {
     /* wait until all 160 lines have been updated */
     while (*scanline_counter < 160) { }
 }
-
-
-/* this function checks whether a particular button has been pressed */
-unsigned char button_pressed(unsigned short button) {
-    /* and the button register with the button constant we want */
-    unsigned short pressed = *buttons & button;
-
-    /* if this value is zero, then it's not pressed */
-    if (pressed == 0) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
 
 /* return a pointer to one of the 4 character blocks (0-3) */
 volatile unsigned short* char_block(unsigned long block) {
@@ -152,9 +108,6 @@ void setup_background() {
         dest[i] = map2[i];
     }
 }
-
-
-/* just kill time */
 void delay(unsigned int amount) {
     for (int i = 0; i < amount * 10; i++);
 }
@@ -167,39 +120,11 @@ int main() {
     /* setup the background 0 */
     setup_background();
 
-    /* set initial scroll to 0 */
-    int xscroll = 0;
-    int yscroll = 0;
-
-    int xscroll2 = 0;
-    int yscroll2 = 0;
 
     /* loop forever */
     while (1) {
-        /* scroll with the arrow keys */
-        if (button_pressed(BUTTON_DOWN)) {
-            yscroll++;
-            yscroll2 +=2;
-        }
-        if (button_pressed(BUTTON_UP)) {
-            yscroll--;
-            yscroll2 -=2;
-        }
-        if (button_pressed(BUTTON_RIGHT)) {
-            xscroll++;
-            xscroll2 +=2;
-        }
-        if (button_pressed(BUTTON_LEFT)) {
-            xscroll--;
-            xscroll2 -=2;
-        }
 
-        /* wait for vblank before scrolling */
         wait_vblank();
-        *bg0_x_scroll = xscroll;
-        *bg0_y_scroll = yscroll;
-        *bg1_x_scroll = xscroll2;  
-        *bg1_y_scroll = yscroll2;  
         /* delay some */
         delay(50);
     }
